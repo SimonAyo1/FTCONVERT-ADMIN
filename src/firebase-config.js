66a446d7435062b88325b469d93026410a30ee4d
simonayo <ayosimon2000@gmail.com>
2023-01-03 10:10:09 +0100

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,7 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -13,4 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
+export const auth = getAuth(app);
